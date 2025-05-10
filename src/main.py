6b6e41929bfdbcd7698f0d98101331c5ce95fd00
Hank Kalashian <chef_hank@MacBook-Air-Hank.local>
2025-05-11 02:28:56 +0300

    мяу

---
+++ src/main.py
@@ -0,0 +1,8 @@
+print("hello world")
+# git inid - инициализация репозитория 
+# git remote add origin https://github.com/hank4354/practice-2025-1
+# git status - проверка на добавление (необязательно)
+# git add . - положил продукты в корзину
+# git commit -m "блабла" - комментарий
+# git push origin main - добавление в репозеторий 
+# git remote -v - проверить в каком репозитории
